93ac4f1abae0ef5e31b9e5f4c911e1b57693d25a
Daniel Shir <daniels@viber.com>
2017-08-24 16:01:44 +0300

    Added simple test to dump file info

---
+++ test/file_info.js
@@ -0,0 +1,12 @@
+const util = require('util');
+const parquet = require('./index');
+
+let filename = '/Users/dshir/Work/Projects/Parquet/comp/ypgpnj-2017-06-21-18-logins.parquet';
+filename = process.argv[2] || filename;
+
+console.log("Using file " + filename);
+
+const p = new parquet.ParquetReader(filename);
+
+console.dir(p.info());
+
