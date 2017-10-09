7bcd09399e57d54aa0f5f15ab0ec5ea249bd0aa2
Daniel Shir <daniels@viber.com>
2017-10-09 15:31:18 +0300

    Fixed import file path

--- test/file_info.js
+++ test/file_info.js
@@ -1,5 +1,5 @@
 const util = require('util');
-const parquet = require('./index');
+const parquet = require(__dirname + '/../index');
 
 let filename = '/Users/dshir/Work/Projects/Parquet/comp/ypgpnj-2017-06-21-18-logins.parquet';
 filename = process.argv[2] || filename;
